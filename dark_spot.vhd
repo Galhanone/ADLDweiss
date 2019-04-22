1daac5a4aa3b08207dc949623b57d73df998b641
Galhanone <d.dan.gar.g@gmail.com>
2019-04-21 22:55:51 -0400

    More work on Dark_Spot

--- dark_spot.vhd
+++ dark_spot.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity dark_spot is
 	port(
 		location 	: in  std_logic_vector(1 DOWNTO 0);
-		right_vfar	: in  std_logic;
+		side_vfar	: in  std_logic;
 		done		: in  std_logic;
 		go, play 	: out std_logic;
 		right_left	: out std_logic);
@@ -20,12 +20,25 @@ begin
 			wait until done = '1';
 			play <= '0';
 			go <= '1';
+			--left_fwd <= '1';
+			--right_fwd <= '1';
 			right_left <= '0';
-			--left wall-follow till out
+			wait until side_vfar = '1';
+			--wait until back_far = '1';
+			--left_fwd <= '0';
+			--wait until side_far = '0';
+			--left_fwd <= '1';
+			--wait until front_close = '1';
 		elsif location = "10" then	--Tunnel
+			go <= '1';
+			--left_fwd <= '1';
+			--right_fwd <= '1';
 			right_left <= '1';
-			wait until right_vfar = '1';
-			--go straight
+			wait until side_vfar = '1';
+			--wait until back_far = '1';
+			--right_fwd <= '0';
+			--wait until side_far = '0';
+			--right_fwd <= '1';
 		elsif location = "11" then	--Church
 			go <= '0';
 			play <= '1';
