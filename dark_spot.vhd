89a4f2af39f4133487dd6d2bae419e06641beafa
Galhanone <d.dan.gar.g@gmail.com>
2019-04-24 17:09:05 -0400

    Dark_Spot partially implemented
    Dark detection done (untested), light detection not done

--- dark_spot.vhd
+++ dark_spot.vhd
@@ -4,31 +4,53 @@ use ieee.std_logic_1164.all;
 entity dark_spot is
 	port(
 		location 	: in  std_logic_vector(1 DOWNTO 0);
+		exit_dark	: in std_logic;
 		done		: in  std_logic;
-		play		: out std_logic;
+		reset_music	: out std_logic;
 		dark_go		: out std_logic;
 		right_left	: out std_logic);
 end dark_spot;
 
 architecture rtl of dark_spot is
 begin
+	--TODO: Does right_left need to be initialized???
 	process(location)
 	begin
 		if location = "01" then		--Friend
+			reset_music <= '1';
 			dark_go <= '0';
-			play <= '1';
-			wait until done = '1';
-			play <= '0';
+			reset_music <= '0';
+			while done = '0' loop
+			end loop;
+			reset_music <= '1';
 			dark_go <= '1';
 			right_left <= '0';
 		elsif location = "10" then	--Tunnel
 			dark_go <= '1';
 			right_left <= '1';
 		elsif location = "11" then	--Church
+			reset_music <= '1';
 			dark_go <= '0';
-			play <= '1';
-			wait until done = '1';
-			play <= '0';
+			reset_music <= '0';
+			while done = '0' loop
+			end loop;
+			reset_music <= '1';
+		end if;
+	end process;
+	
+	process(exit_dark)
+	begin
+		if location = "01" then		--Friend
+			--stop
+			--turn left
+			--wait until side_close*
+			--go forward ignoring wall
+			--wait until front_close*
+			--carry on with left wall follow
+		elsif location = "10" then	--Tunnel
+			--ignore sides and go straight
+			--wait until front_close*
+			--carry on with right wall follow
 		end if;
 	end process;
 end rtl;
